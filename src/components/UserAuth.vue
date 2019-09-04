23aa97d79fba907918896e007b3a658087ebca31
Steven <s.millot@neolynk.fr>
2019-09-04 20:52:41 +0200

    Remove unused style

--- src/components/UserAuth.vue
+++ src/components/UserAuth.vue
@@ -54,19 +54,4 @@ export default {
     margin: 1rem;
   }
 }
-
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
